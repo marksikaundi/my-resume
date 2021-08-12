119b400d217030f40f0e75d265775e0c458fb4df
Mark Sikaundi <sikaundimark@gmail.com>
2021-08-12 11:57:29 +0200

    On branch master

---
+++ assets/scripts/app.js
@@ -0,0 +1,84 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "React Native",
+				weight: 12.3
+			}, {
+				text: "css3",
+				weight: 8
+			}, {
+				text: "javascript",
+				weight: 14
+			}, {
+				text: "Tailwind-css",
+				weight: 3
+			}, {
+				text: "Applications",
+				weight: 7
+			}, {
+				text: "SQL",
+				weight: 10
+			}, {
+				text: "HTML5",
+				weight: 9
+			}, {
+				text: "Python",
+				weight: 15
+			}, {
+				text: "ML",
+				weight: 10
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Data scientist & Full-Stack Web Developer.", "love everything about ICT Industry.", "also teach programming to people.", "I'm a computer scientist."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#000"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
